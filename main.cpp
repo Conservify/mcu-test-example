7aeb991a98a85d5ecdc6f5478b91264cbde4fb8e
Jacob Lewallen <jlewallen@gmail.com>
2018-08-11 11:58:16 -0700

    First commit. Just writes to serial.

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+void setup() {
+    Serial.begin(115200);
+
+    while (!Serial) {
+        delay(10);
+    }
+
+    Serial.println("Ready");
+}
+
+void loop() {
+    for (auto i = 0; i < 10; ++i) {
+        Serial.println("Tick");
+        delay(100);
+    }
+}
